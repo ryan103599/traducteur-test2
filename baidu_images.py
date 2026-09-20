94150cc00d2d3598dc347c2f62b54a937dfca314
ryan103599 <levirousseaux@gmail.com>
2026-09-20 20:46:19 +0200

    Met à jour l'intégration Baidu Image Translation V2.0

--- baidu_images.py
+++ baidu_images.py
@@ -7,7 +7,7 @@ import requests
 from PIL import Image
 
 
-BAIDU_IMAGE_URL = "https://aip.baidubce.com/file/2.0/mt/pictrans/v1"
+# Baidu Image Translation API-V2.0.\n# The current Baidu V2.0 product uses this public endpoint; its internal path remains /v1.\nBAIDU_IMAGE_URL = "https://aip.baidubce.com/file/2.0/mt/pictrans/v1"
 
 TARGET_TO_BAIDU = {
     "fr": "fra",
